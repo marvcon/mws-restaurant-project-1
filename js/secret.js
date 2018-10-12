36058698a33caa58632ea89b5ab4cbda55e89b9e
marvcon <marvinlcon@gmail.com>
2018-10-11 21:07:53 -0500

    Enable mapbox maps, but keep API key secret

---
+++ js/secret.js
@@ -0,0 +1,7 @@
+ // js/secret.js
+class SECRET {
+    static get mapbox_key() {
+      // return your mapbox API key
+      return "pk.eyJ1IjoibWFydmNvbiIsImEiOiJjamljZzdvNmcwMG8wM2txdjFsZnNhanNqIn0.ienXnxJURNO-1UFDdagzqg";
+    }
+  }
